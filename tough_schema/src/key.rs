d172c06ed989da0c7cc744242e6d3feff5e71a84
iliana destroyer of worlds <iweller@amazon.com>
2019-08-27 20:15:08 +0000

    Update to latest dependent crates
    Signed-off-by: iliana destroyer of worlds <iweller@amazon.com>

--- tough_schema/src/key.rs
+++ tough_schema/src/key.rs
@@ -86,8 +86,7 @@ impl Key {
             ),
         };
 
-        ring::signature::verify(
-            alg,
+        alg.verify(
             public_key,
             untrusted::Input::from(msg),
             untrusted::Input::from(signature),
