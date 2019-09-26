4bdf3496ed9524c3a972561eaef5509ab5a6bd5d
David Barsky <dbarsky@amazon.com>
2019-09-26 23:53:30 +0000

    add #![deny(rust_2018_idioms)]

--- tough_schema/src/lib.rs
+++ tough_schema/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+#![deny(rust_2018_idioms)]
 #![warn(clippy::pedantic)]
 #![allow(clippy::module_name_repetitions)]
 
--- tuftool/src/main.rs
+++ tuftool/src/main.rs
@@ -1,6 +1,7 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+#![deny(rust_2018_idioms)]
 #![warn(clippy::pedantic)]
 
 mod copylike;
