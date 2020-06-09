358c4607a499d12d8746b5f6e6d546dc0d7932f8
Zac Mrowicki <mrowicki@amazon.com>
2020-06-09 21:53:32 +0000

    Unify tuftool `create` and `update` cli interfaces
    Previously `create`, `refresh`, and `update` all had slightly
    different interfaces. This commit fixes up the `create` command
    to make it uniform with `update`.

--- tuftool/tests/update_command.rs
+++ tuftool/tests/update_command.rs
@@ -6,12 +6,11 @@ mod utl;
 use assert_cmd::Command;
 use chrono::{Duration, Utc};
 use std::fs::File;
+use std::path::Path;
 use tempfile::TempDir;
 use tough::{ExpirationEnforcement, Limits, Repository, Settings};
 
-#[test]
-// Ensure we can read a repo that has been updated by `tuftool update`
-fn update_command() {
+fn create_repo<P: AsRef<Path>>(repo_dir: P) {
     let timestamp_expiration = Utc::now().checked_add_signed(Duration::days(1)).unwrap();
     let timestamp_version: u64 = 31;
     let snapshot_expiration = Utc::now().checked_add_signed(Duration::days(2)).unwrap();
@@ -21,15 +20,16 @@ fn update_command() {
     let targets_input_dir = utl::test_data().join("tuf-reference-impl").join("targets");
     let root_json = utl::test_data().join("simple-rsa").join("root.json");
     let root_key = utl::test_data().join("snakeoil.pem");
-    let repo_dir = TempDir::new().unwrap();
 
     // Create a repo using tuftool and the reference tuf implementation data
     Command::cargo_bin("tuftool")
         .unwrap()
         .args(&[
             "create",
+            "-t",
             targets_input_dir.to_str().unwrap(),
-            repo_dir.path().to_str().unwrap(),
+            "-o",
+            repo_dir.as_ref().to_str().unwrap(),
             "-k",
             root_key.to_str().unwrap(),
             "--root",
@@ -49,6 +49,96 @@ fn update_command() {
         ])
         .assert()
         .success();
+}
+
+#[test]
+// Ensure we can read a repo that has had its metadata updated by `tuftool create`
+fn update_command_without_new_targets() {
+    let root_json = utl::test_data().join("simple-rsa").join("root.json");
+    let root_key = utl::test_data().join("snakeoil.pem");
+    let repo_dir = TempDir::new().unwrap();
+
+    // Create a repo using tuftool and the reference tuf implementation data
+    create_repo(repo_dir.path());
+
+    // Set new expiration dates and version numbers for the update command
+    let new_timestamp_expiration = Utc::now().checked_add_signed(Duration::days(4)).unwrap();
+    let new_timestamp_version: u64 = 310;
+    let new_snapshot_expiration = Utc::now().checked_add_signed(Duration::days(5)).unwrap();
+    let new_snapshot_version: u64 = 250;
+    let new_targets_expiration = Utc::now().checked_add_signed(Duration::days(6)).unwrap();
+    let new_targets_version: u64 = 170;
+    let metadata_base_url = &utl::dir_url(repo_dir.path().join("metadata"));
+    let update_out = TempDir::new().unwrap();
+
+    // Update the repo we just created
+    Command::cargo_bin("tuftool")
+        .unwrap()
+        .args(&[
+            "update",
+            "-o",
+            update_out.path().to_str().unwrap(),
+            "-k",
+            root_key.to_str().unwrap(),
+            "--root",
+            root_json.to_str().unwrap(),
+            "--metadata-url",
+            metadata_base_url,
+            "--targets-expires",
+            new_targets_expiration.to_rfc3339().as_str(),
+            "--targets-version",
+            format!("{}", new_targets_version).as_str(),
+            "--snapshot-expires",
+            new_snapshot_expiration.to_rfc3339().as_str(),
+            "--snapshot-version",
+            format!("{}", new_snapshot_version).as_str(),
+            "--timestamp-expires",
+            new_timestamp_expiration.to_rfc3339().as_str(),
+            "--timestamp-version",
+            format!("{}", new_timestamp_version).as_str(),
+        ])
+        .assert()
+        .success();
+
+    // Load the updated repo
+    let temp_datastore = TempDir::new().unwrap();
+    let updated_metadata_base_url = &utl::dir_url(update_out.path().join("metadata"));
+    let updated_targets_base_url = &utl::dir_url(update_out.path().join("targets"));
+    let repo = Repository::load(
+        &tough::FilesystemTransport,
+        Settings {
+            root: File::open(root_json).unwrap(),
+            datastore: temp_datastore.as_ref(),
+            metadata_base_url: updated_metadata_base_url,
+            targets_base_url: updated_targets_base_url,
+            limits: Limits::default(),
+            expiration_enforcement: ExpirationEnforcement::Safe,
+        },
+    )
+    .unwrap();
+
+    // Ensure all the existing targets are accounted for
+    assert_eq!(repo.targets().signed.targets.len(), 3);
+
+    // Ensure all the metadata has been updated
+    assert_eq!(repo.targets().signed.version.get(), new_targets_version);
+    assert_eq!(repo.targets().signed.expires, new_targets_expiration);
+    assert_eq!(repo.snapshot().signed.version.get(), new_snapshot_version);
+    assert_eq!(repo.snapshot().signed.expires, new_snapshot_expiration);
+    assert_eq!(repo.timestamp().signed.version.get(), new_timestamp_version);
+    assert_eq!(repo.timestamp().signed.expires, new_timestamp_expiration);
+}
+
+#[test]
+// Ensure we can read a repo that has had its metadata and targets updated
+// by `tuftool create`
+fn update_command_with_new_targets() {
+    let root_json = utl::test_data().join("simple-rsa").join("root.json");
+    let root_key = utl::test_data().join("snakeoil.pem");
+    let repo_dir = TempDir::new().unwrap();
+
+    // Create a repo using tuftool and the reference tuf implementation data
+    create_repo(repo_dir.path());
 
     // Set new expiration dates and version numbers for the update command
     let new_timestamp_expiration = Utc::now().checked_add_signed(Duration::days(4)).unwrap();
