b44413c85a4800bfb33af3bb3e2ba5a72604de87
Matt Briggs <brigmatt@amazon.com>
2020-07-13 15:29:56 -0700

    tuftool: truncate targets when downloading

---
+++ tuftool/tests/download_command.rs
@@ -0,0 +1,107 @@
+mod test_utils;
+
+use assert_cmd::Command;
+use mockito::mock;
+use std::fs::{read_to_string, OpenOptions};
+use std::io::Write;
+use std::str::FromStr;
+use tempfile::TempDir;
+use url::Url;
+
+/// Create a path in a mock HTTP server which serves a file from `tuf-reference-impl`.
+fn create_successful_get_mock(relative_path: &str) -> mockito::Mock {
+    let repo_dir = test_utils::test_data().join("tuf-reference-impl");
+    let file_bytes = std::fs::read(&repo_dir.join(relative_path)).unwrap();
+    mock("GET", ("/".to_owned() + relative_path).as_str())
+        .with_status(200)
+        .with_header("content-type", "application/octet-stream")
+        .with_body(file_bytes.as_slice())
+        .create()
+}
+
+/// Asserts that the named file in `outdir` exactly matches the file in `tuf-reference-impl/targets`
+fn assert_file_match(outdir: &TempDir, filename: &str) {
+    let got = read_to_string(outdir.path().join(filename)).unwrap();
+    let want = read_to_string(
+        test_utils::test_data()
+            .join("tuf-reference-impl")
+            .join("targets")
+            .join(filename),
+    )
+    .unwrap();
+    assert_eq!(got, want, "{} contents do not match.", filename);
+}
+
+#[test]
+// Ensure that the download command works, and that we truncate files when downloading into a non-
+// empty directory (i.e. that issue #173 is fixed).
+fn download_command_truncates() {
+    // let repo_dir = utl::test_data().join("tuf-reference-impl");
+    let _role_1 = create_successful_get_mock("metadata/role1.json");
+    let _role_2 = create_successful_get_mock("metadata/role2.json");
+    let _snapshot = create_successful_get_mock("metadata/snapshot.json");
+    let _targets = create_successful_get_mock("metadata/targets.json");
+    let _timestamp = create_successful_get_mock("metadata/timestamp.json");
+    let _file1 = create_successful_get_mock("targets/file1.txt");
+    let _file2 = create_successful_get_mock("targets/file2.txt");
+    let _file3 = create_successful_get_mock("targets/file3.txt");
+    let base_url = Url::from_str(mockito::server_url().as_str()).unwrap();
+    let metadata_base_url = base_url.join("metadata").unwrap().to_string();
+    let targets_base_url = base_url.join("targets").unwrap().to_string();
+
+    let outdir = TempDir::new().unwrap();
+    let root_json = test_utils::test_data()
+        .join("tuf-reference-impl")
+        .join("metadata")
+        .join("root.json");
+
+    // Download a test repo.
+    Command::cargo_bin("tuftool")
+        .unwrap()
+        .args(&[
+            "download",
+            "-r",
+            root_json.to_str().unwrap(),
+            "--metadata-url",
+            metadata_base_url.as_str(),
+            "--target-url",
+            targets_base_url.as_str(),
+            outdir.path().to_str().unwrap(),
+        ])
+        .assert()
+        .success();
+
+    // Assert the files are exactly correct
+    assert_file_match(&outdir, "file1.txt");
+    assert_file_match(&outdir, "file2.txt");
+    // TODO - assert_file_match(&outdir, "file3.txt"); when delegate support lands.
+
+    // Add "bloop" to the end of file1.txt so that we can prove that the file is truncated when we
+    // download the repo a second time into the same outdir.
+    let mut f = OpenOptions::write(&mut OpenOptions::new(), true)
+        .append(true)
+        .open(outdir.path().join("file1.txt"))
+        .unwrap();
+    writeln!(f, "bloop").unwrap();
+
+    // Download again into the same outdir
+    Command::cargo_bin("tuftool")
+        .unwrap()
+        .args(&[
+            "download",
+            "-r",
+            root_json.to_str().unwrap(),
+            "--metadata-url",
+            metadata_base_url.as_str(),
+            "--target-url",
+            targets_base_url.as_str(),
+            outdir.path().to_str().unwrap(),
+        ])
+        .assert()
+        .success();
+
+    // Assert the files are exactly correct
+    assert_file_match(&outdir, "file1.txt");
+    assert_file_match(&outdir, "file2.txt");
+    // TODO - assert_file_match(&outdir, "file3.txt"); when delegate support lands.
+}
--- tuftool/tests/test_utils.rs
+++ tuftool/tests/test_utils.rs
@@ -5,7 +5,10 @@ use std::io::Read;
 use std::path::{Path, PathBuf};
 use url::Url;
 
+/// Utilities for tests. Not every test module uses every function, so we suppress unused warnings.
+
 /// Returns the path to our test data directory
+#[allow(unused)]
 pub fn test_data() -> PathBuf {
     let mut p = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     p.pop();
@@ -13,11 +16,13 @@ pub fn test_data() -> PathBuf {
 }
 
 /// Converts a filepath into a URI formatted string
+#[allow(unused)]
 pub fn dir_url<P: AsRef<Path>>(path: P) -> String {
     Url::from_directory_path(path).unwrap().to_string()
 }
 
 /// Returns a vector of bytes from any object with the Read trait
+#[allow(unused)]
 pub fn read_to_end<R: Read>(mut reader: R) -> Vec<u8> {
     let mut v = Vec::new();
     reader.read_to_end(&mut v).unwrap();
