132f32b10df292921a2b27ec7843e7381a09e293
Jonas <Jonas-Zunker@web.de>
2023-08-22 00:08:21 +0200

    Fix whitespace fmt

--- tough/src/target_name.rs
+++ tough/src/target_name.rs
@@ -137,7 +137,7 @@ fn clean_name(name: &str) -> Result<String> {
                 .context(error::TargetNameComponentsEmptySnafu { name })?
                 .as_os_str();
 
-            // If the first component isn't the main separator ( unix `/`, windows '\' ) 
+            // If the first component isn't the main separator ( unix `/`, windows '\' )
             // then there is a bug or behavior change in absolutize_from.
             ensure!(
                 first_component == std::path::MAIN_SEPARATOR_STR,
