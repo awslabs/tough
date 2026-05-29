75bc42815ca4c1a83271479ea1af1912f7956f37
Emily Albini <emily@oxidecomputer.com>
2026-05-29 14:55:06 +0200

    deduplicate signature verification logic

--- tough/src/schema/verify.rs
+++ tough/src/schema/verify.rs
@@ -1,9 +1,11 @@
 use super::error::{self, Result};
-use super::{Delegations, Role, RoleType, Root, Signed, Targets};
+use super::{Delegations, Role, Root, Signed, Targets};
+use crate::schema::key::{Key, KeyId};
 use olpc_cjson::CanonicalFormatter;
 use serde::Serialize;
 use snafu::{ensure, OptionExt, ResultExt};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::num::NonZeroU64;
 
 impl Root {
     /// Checks that the given metadata role is valid based on a threshold of key signatures.
@@ -12,49 +14,14 @@ impl Root {
             .roles
             .get(&T::TYPE)
             .context(error::MissingRoleSnafu { role: T::TYPE })?;
-        let mut valid = 0;
 
-        let mut data = Vec::new();
-        let mut ser = serde_json::Serializer::with_formatter(&mut data, CanonicalFormatter::new());
-        role.signed
-            .serialize(&mut ser)
-            .context(error::JsonSerializationSnafu {
-                what: format!("{} role", T::TYPE),
-            })?;
-
-        let mut valid_keyids = HashSet::new();
-        let mut contained_keyids = HashSet::new();
-
-        for signature in &role.signatures {
-            ensure!(
-                !contained_keyids.contains(&signature.keyid),
-                error::DuplicateKeyIdSnafu {
-                    keyid: signature.keyid.clone(),
-                }
-            );
-            contained_keyids.insert(&signature.keyid);
-            if role_keys.keyids.contains(&signature.keyid) {
-                if let Some(key) = self.keys.get(&signature.keyid) {
-                    if key.verify(&data, &signature.sig) {
-                        // we have ensured that this keyid is not already
-                        // present in valid_keyids with the test on
-                        // contained_keyids.
-                        valid_keyids.insert(&signature.keyid);
-                        valid += 1;
-                    }
-                }
-            }
-        }
-
-        ensure!(
-            valid >= u64::from(role_keys.threshold),
-            error::SignatureThresholdSnafu {
-                role: T::TYPE,
-                threshold: role_keys.threshold,
-                valid,
-            }
-        );
-        Ok(())
+        verify_common(
+            &self.keys,
+            role,
+            &T::TYPE.to_string(),
+            &role_keys.keyids,
+            role_keys.threshold,
+        )
     }
 }
 
@@ -68,50 +35,71 @@ impl Delegations {
                 .ok_or(error::Error::RoleNotFound {
                     name: name.to_string(),
                 })?;
-        let mut valid = 0;
 
-        // serialize the role to verify the key ID by using the JSON representation
-        let mut data = Vec::new();
-        let mut ser = serde_json::Serializer::with_formatter(&mut data, CanonicalFormatter::new());
-        role.signed
-            .serialize(&mut ser)
-            .context(error::JsonSerializationSnafu {
-                what: format!("{name} role"),
-            })?;
-        let mut valid_keyids = HashSet::new();
-        let mut contained_keyids = HashSet::new();
+        verify_common(
+            &self.keys,
+            role,
+            name,
+            &role_keys.keyids,
+            role_keys.threshold,
+        )
+    }
+}
 
-        for signature in &role.signatures {
-            ensure!(
-                !contained_keyids.contains(&signature.keyid),
-                error::DuplicateKeyIdSnafu {
-                    keyid: signature.keyid.clone(),
-                }
-            );
-            contained_keyids.insert(&signature.keyid);
-            if role_keys.keyids.contains(&signature.keyid) {
-                if let Some(key) = self.keys.get(&signature.keyid) {
-                    if key.verify(&data, &signature.sig) {
-                        // we have ensured that this keyid is not already
-                        // present in valid_keyids with the test on
-                        // contained_keyids.
-                        valid_keyids.insert(&signature.keyid);
-                        valid += 1;
-                    }
+fn verify_common<T: Role + Serialize>(
+    keys: &HashMap<KeyId, Key>,
+    role: &Signed<T>,
+    role_name: &str,
+    role_keys: &[KeyId],
+    threshold: NonZeroU64,
+) -> Result<()> {
+    let mut valid = 0;
+
+    let mut data = Vec::new();
+    let mut ser = serde_json::Serializer::with_formatter(&mut data, CanonicalFormatter::new());
+    role.signed
+        .serialize(&mut ser)
+        .context(error::JsonSerializationSnafu {
+            what: format!("{role_name} role"),
+        })?;
+
+    let mut valid_keyids = HashSet::new();
+    let mut contained_keyids = HashSet::new();
+
+    for signature in &role.signatures {
+        let keyid = &signature.keyid;
+
+        ensure!(
+            !contained_keyids.contains(keyid),
+            error::DuplicateKeyIdSnafu {
+                keyid: keyid.clone(),
+            }
+        );
+        contained_keyids.insert(keyid);
+
+        if role_keys.contains(keyid) {
+            if let Some(key) = keys.get(keyid) {
+                if key.verify(&data, &signature.sig) {
+                    // we have ensured that this keyid is not already
+                    // present in valid_keyids with the test on
+                    // contained_keyids.
+                    valid_keyids.insert(keyid);
+                    valid += 1;
                 }
             }
         }
-
-        ensure!(
-            valid >= u64::from(role_keys.threshold),
-            error::SignatureThresholdSnafu {
-                role: RoleType::Targets,
-                threshold: role_keys.threshold,
-                valid,
-            }
-        );
-        Ok(())
     }
+
+    ensure!(
+        valid >= u64::from(threshold),
+        error::SignatureThresholdSnafu {
+            role: T::TYPE,
+            threshold,
+            valid,
+        }
+    );
+
+    Ok(())
 }
 
 #[cfg(test)]
