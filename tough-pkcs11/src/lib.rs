51a2442e7fecd2522ca782397649fd15f7dc2ac2
G1gg1L3s <nazar.serhiichuk@cossacklabs.com>
2026-07-19 16:37:37 +0300

    tough-pkcs11: Implement KeySource and Key traits

--- tough-pkcs11/src/lib.rs
+++ tough-pkcs11/src/lib.rs
@@ -1,14 +1,37 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2026 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 pub mod error;
 mod signer;
 
-use tough::async_trait;
+use aws_lc_rs::rand::SecureRandom;
+use snafu::ResultExt;
 use tough::key_source::KeySource;
 use tough::sign::Sign;
+use tough::{async_trait, schema::key::Key};
 
-pub use signer::{KeyId, Pkcs11KeySource, TokenId};
+pub use signer::{KeyId, Pkcs11Key, Pkcs11KeySource, TokenId};
+
+#[async_trait]
+impl Sign for Pkcs11Key {
+    fn tuf_key(&self) -> Key {
+        self.pub_key()
+    }
+
+    async fn sign(
+        &self,
+        msg: &[u8],
+        _rng: &(dyn SecureRandom + Sync),
+    ) -> core::result::Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync + 'static>> {
+        let this = self.clone();
+        let msg = msg.to_vec();
+        let signature = tokio::task::spawn_blocking(move || this.sign(&msg))
+            .await
+            .context(error::JoinSpawnBlockingTaskSnafu)??;
+
+        Ok(signature)
+    }
+}
 
 /// Implements the KeySource trait.
 #[async_trait]
@@ -17,7 +40,12 @@ impl KeySource for Pkcs11KeySource {
         &self,
     ) -> std::result::Result<Box<dyn Sign>, Box<dyn std::error::Error + Send + Sync + 'static>>
     {
-        todo!()
+        let this = self.clone();
+        let key = tokio::task::spawn_blocking(move || this.to_key())
+            .await
+            .context(error::JoinSpawnBlockingTaskSnafu)??;
+
+        Ok(Box::new(key))
     }
 
     async fn write(
@@ -25,6 +53,6 @@ impl KeySource for Pkcs11KeySource {
         _value: &str,
         _key_id_hex: &str,
     ) -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-        todo!()
+        Err(error::WritingUnsupportedError.into())
     }
 }
